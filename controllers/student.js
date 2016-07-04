1b4e8efccd57c6b2897a4da52522405d9632804d
Apoorv Umang <apoorvumang@gmail.com>
2016-07-04 21:33:54 +0530

    testing

--- controllers/student.js
+++ controllers/student.js
@@ -18,45 +18,45 @@ exports.getResult = (req, res) => {
 }
 
 exports.getStudents = (req, res) => {
-  // numStudents = 9;
-  // queries = [];
-  // for (var i = 0; i < numStudents; i++) {
-  //   queries.push(function (cb) {
-  //     Student.find({rollno: /^11/,rand:{$gte:Math.random()}}, {_id:0}).sort({rand:1}).limit(1).exec(function (err, docs) {
-  //         if (err) {
-  //             throw cb(err);
-  //         }
-  //         // do some stuff with docs & pass or directly pass it
-  //         if (!docs.length) {
-  //             // do stuff here
-  //           Student.find({rollno: /^11/,rand:{$lte:Math.random()}}, {_id:0}).sort({rand:1}).limit(1).exec(function (err, docs) {
-  //             if (err) {
-  //               throw cb(err);
-  //             }
-  //             cb(null, docs[0]);
-  //           });
-  //         }
-  //         else {
-  //           cb(null, docs[0]);
-  //         }
+  numStudents = 9;
+  queries = [];
+  for (var i = 0; i < numStudents; i++) {
+    queries.push(function (cb) {
+      Student.find({rollno: /^11/,rand:{$gte:Math.random()}}, {_id:0}).sort({rand:1}).limit(1).exec(function (err, docs) {
+          if (err) {
+              throw cb(err);
+          }
+          // do some stuff with docs & pass or directly pass it
+          if (!docs.length) {
+              // do stuff here
+            Student.find({rollno: /^11/,rand:{$lte:Math.random()}}, {_id:0}).sort({rand:1}).limit(1).exec(function (err, docs) {
+              if (err) {
+                throw cb(err);
+              }
+              cb(null, docs[0]);
+            });
+          }
+          else {
+            cb(null, docs[0]);
+          }
           
-  //     });
-  //   })   
-  // }
+      });
+    })   
+  }
 
 
-  // async.parallel(queries, function(err, docs) {
-  //     // if any query fails
-  //     if (err) {
-  //         throw err;
-  //     }
-  //     // var merged = [].concat.apply([], docs);
-  //     // console.log(docs[0]);
-  //     res.render('students', { students: docs });
-  // })
+  async.parallel(queries, function(err, docs) {
+      // if any query fails
+      if (err) {
+          throw err;
+      }
+      // var merged = [].concat.apply([], docs);
+      // console.log(docs[0]);
+      res.render('students', { students: docs });
+  })
 
-  Student.findOne({rollno:'11CS30005'}, function(err, docs) {
-    res.send(docs);
-  });
+  // Student.findOne({rollno:'11CS30005'}, function(err, docs) {
+  //   res.send(docs);
+  // });
 
 };
